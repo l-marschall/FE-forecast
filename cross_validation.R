20b0b54c1215b4bc0151c0978752e4650d8d406a
l-marschall <laurits.marschall@barcelonagse.eu>
2018-03-05 18:30:48 +0100

    lm: added if condition for cross_validation function

--- cross_validation.R
+++ cross_validation.R
@@ -2,13 +2,17 @@
 
 cross_mse = function(prediction, cross_periods, df) {
   p <- length(prediction)
-  prediction <- prediction[(p-cross_periods):p]
-  target <- df[,1]
-  n <- length(target)
-  target <- target[(n - cross_periods):n]
-  mse <- rep(0, p)
-  for (i in 1:cross_periods) {
-    mse[i] <- (target[i] - prediction[i])**2 
+  if (cross_periods < p) {
+    prediction <- prediction[(p-cross_periods):p]
+    target <- df[,1]
+    n <- length(target)
+    target <- target[(n - cross_periods):n]
+    mse <- rep(0, p)
+    for (i in 1:cross_periods) {
+      mse[i] <- (target[i] - prediction[i])**2 
+    }
+    avg_mse <- mean(mse)
+    print(avg_mse)
   }
-  avg_mse <- mean(mse)
+  else print('More cross_periods selected than available prediction periods!')
 }
