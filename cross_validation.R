35897b0474ee2ad3a6952aa1bc691706260f93bf
l-marschall <laurits.marschall@barcelonagse.eu>
2018-03-05 18:18:11 +0100

    final cross_validation function

--- cross_validation.R
+++ cross_validation.R
@@ -1,12 +1,17 @@
 rm(list = ls())
 setwd('/home/laurits/Desktop/BGSE/Term 2/Financial Econometrics/FE-forecast')
+df <- read.csv('forecast-competition-training.csv')
 
 
-cross_periods <- length(prediction)
-mse <- rep(0, cross_periods)
-  
-for (i in 1:cross_periods) {
-  mse[i] <- (target[i] - prediction[i])**2 
+cross_mse = function(prediction, cross_periods, df) {
+  p <- length(prediction)
+  prediction <- prediction[(p-cross_periods):p]
+  target <- df[,1]
+  n <- length(target)
+  target <- target[(n - cross_periods):n]
+  mse <- rep(0, p)
+  for (i in 1:cross_periods) {
+    mse[i] <- (target[i] - prediction[i])**2 
+  }
+  avg_mse <- mean(mse)
 }
-
-avg_mse <- mean(mse)
