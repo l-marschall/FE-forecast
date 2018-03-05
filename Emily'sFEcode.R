89675c97c6f6c8834bfc2859a2e9aaeb8f661e5b
Emily <emilydaykin31@gmail.com>
2018-03-05 19:11:41 +0100

    best model

--- Emily'sFEcode.R
+++ Emily'sFEcode.R
@@ -9,93 +9,136 @@ df <- read.csv("forecast-competition-training.csv") # training data
 head(df)
 View(df)
 
-# ----------------------------------- Splitting Train:Test ----------------------------------- #
-#n <- floor(0.90 * nrow(df))
-#set.seed(123)
-#trainIndex <- sample(seq_len(nrow(df)), size = n)
-#trainIndex <- createDataPartition(df$TARGET, p=0.90, list=FALSE)
-#df_train2 <- df[trainIndex, ]
-#df_test2 <- df[-trainIndex, ]
-T <- nrow(df)
-tr <- T - 50
-df_train <- df[1:tr,]
-df_test <- df[(tr+1):T,]
+
 # -------------------------------------- ARMA(1,1) Model -------------------------------------- #
 
-arma11 <- arima(df_train$TARGET, order = c(1,0,1))
-arma11
-target_pred_arma11 <- predict(arma11, newdata = df_test)
-
-target_bench_arma11  <- rep(mean(df_train$TARGET, dim(df_test)[1])) # vector of in_sample y's
-
-###
-H <- nrow(df_test)
-N <- nrow(df_train)-H
-
-# ARMA(1)
-arma11_predicted <- predict(arma11, n.ahead=1)
-arma11_mse <- (mean((df_test - as.numeric(arma11_predicted$pred))**2))
-arma11_mse
-###
-
-arma11_pred <- rep(0,H)
-
-for( m in 0:(H-1) ){
-  y <- df_train[1:(N+m),2]
-  arma11 = arima(y,order=c(1,0,1))
-  arma11_pred[1+m] <- predict( arma11 , n.ahead=1 )$pred
-}
-y
-
-
-train_control <- trainControl(method="repeatedcv", number=10, repeats=3)
-# train the model
-modelarma11 <- train(arma11, data=df_train2, trControl=train_control)
-# summarize results
-print(model)
-
-e_0_arma11 <- df_test$TARGET - target_bench_arma11  # demeaned
-e_1_arma11 <- df_test$TARGET - target_pred_arma11    # 
-
-mse_0_arma11 <- mean(e_0_arma11**2)        
-mse_1_arma11 <- mean(e_1_arma11**2)
-      
-R2_0_arma11 <- 1 - mse_0_arma11/var(df_test$TARGET)
-R2_1_arma11 <- 1 - mse_1_arma11/var(df_test$TARGET)
-
-# -------------------------------------- TARCH Model -------------------------------------- #
-
-
-t.n <- Tgarch11(df_train$TARGET[1:tr])
-Q12 <- round(c(t.n$par["omega"],t.n$par["alpha"],t.n$par["gam1"],t.n$par["beta"]),3)
-
-
-
-
-
-
-
-# ------------------------------------------------------------------------------------- #
-
-
-
-
-## --------------------------- CB's code example --------------------------- ##
-
-predictor.one <- function(data){          # input data is the txN df up to time t
-  T <- nrow(data)
-  y.in <- data[2:T,1]
-  X.in <- data[1:(T-1),1]
-  X.out<- data[T,1]
-  mdl <- lm( y.in ~ X.in )
-  beta <- coef(mdl)
-  f <- beta[1] + beta[2]*X.out[1]
-  df_train$TARGETurn( f )                            # f is the scalar forecast for t+1
+# ARMA11
+target_pred1 <- function(x){
+  arma11 <- arima(x$TARGET, order = c(1,0,1))
+  pred_arma11 <- predict(arma11, n.ahead=1)
+  forecast <- pred_arma11$pred
+  return(forecast)
 }
 
-## ------------------------------------------------------------------------ ##
+# ARMA22
+target_pred2 <- function(x){
+  arma11 <- arima(x$TARGET, order = c(0,0,2))
+  pred_arma11 <- predict(arma11, n.ahead=1)
+  forecast <- pred_arma11$pred
+  return(forecast)
+}
 
-CREATIVE PART
- VECM
- xLASSO
- Markov Chain/HMM
+# ARMA33
+target_pred3 <- function(x){
+  arma11 <- arima(x$TARGET, order = c(1,0,3))
+  pred_arma11 <- predict(arma11, n.ahead=1)
+  forecast <- pred_arma11$pred
+  return(forecast)
+}
+
+forecasts1 <- c()
+for (i in 1:50){
+  train <- df[i:(i+449),]
+  forecasts1[i] <- target_pred1(train)
+}
+
+forecasts2 <- c()
+for (i in 1:50){
+  train <- df[i:(i+449),]
+  forecasts2[i] <- target_pred2(train)
+}
+
+forecasts3 <- c()
+for (i in 1:50){
+  train <- df[i:(i+449),]
+  forecasts3[i] <- target_pred3(train)
+}
+
+forecasts
+
+source("cross_validation.R")
+
+cross_mse(forecasts1, 50, df)
+cross_mse(forecasts2, 50, df)
+cross_mse(forecasts3, 50, df)
+
+# -------------------------- Rolling (50 periods) -------------------------- #
+# ARMA(1,1): MSE=0.631
+# ARMA(2,2): MSE=0.593
+    # AR(2): MSE=0.562          #
+    # MA(2): MSE=0.864
+    # ARMA(2,1): MSE=0.5894
+    # ARMA(1,2): MSE=0.5895
+# ARMA(3,3): MSE=0.601
+    # AR(3): MSE=0.585
+    # ARMA(3,1): MSE=0.584     
+    # ARMA(3,2): MSE=0.589
+    # MA(3): MSE=0.794
+    # ARMA(1,3): MSE=0.0.586       
+    # ARMA(2,3): MSE=0.593
+# ARIMA(1,1,1): MSE=0.576
+# ARIMA(2,1,2): MSE=0.616
+
+# -------------------------------------- ARMA(1,1) Model -------------------------------------- #
+
+# ARMA11
+target_pred01 <- function(x){
+  arma11 <- arima(x$TARGET, order = c(1,0,1))
+  pred_arma11 <- predict(arma11, n.ahead=1)
+  forecast <- pred_arma11$pred
+  return(forecast)}
+
+# ARMA22
+target_pred02 <- function(x){
+  arma11 <- arima(x$TARGET, order = c(0,0,2))
+  pred_arma11 <- predict(arma11, n.ahead=1)
+  forecast <- pred_arma11$pred
+  return(forecast)}
+
+# ARMA33
+target_pred03 <- function(x){
+  arma11 <- arima(x$TARGET, order = c(3,0,3))
+  pred_arma11 <- predict(arma11, n.ahead=1)
+  forecast <- pred_arma11$pred
+  return(forecast)}
+
+forecasts01 <- c()
+for (i in 1:50){
+  train <- df[1:(i+449),]
+  forecasts01[i] <- target_pred01(train)}
+
+forecasts02 <- c()
+for (i in 1:50){
+  train <- df[1:(i+449),]
+  forecasts02[i] <- target_pred02(train)}
+
+forecasts03 <- c()
+for (i in 1:50){
+  train <- df[1:(i+449),]
+  forecasts03[i] <- target_pred03(train)
+}
+
+
+cross_mse(forecasts01, 50, df)
+cross_mse(forecasts02, 50, df)
+cross_mse(forecasts03, 50, df)
+
+
+
+# -------------------------- NOT Rolling (50 periods) -------------------------- #
+
+# ARMA(1,1): MSE=0.608
+# ARMA(2,2): MSE=0.567        
+    # ARMA(2,1): MSE=0.572
+    # ARMA(1,2): MSE=0.573
+    # AR(2): MSE=0.557        #
+    # MA(2): MSE=0.849
+# ARMA(3,3): MSE=0.597
+    # AR(3): MSE=0.573
+    # ARMA(3,1): MSE=0.570     
+    # ARMA(3,2): MSE=0.571
+    # MA(3): MSE=0.779
+    # ARMA(1,3): MSE=0.570       
+    # ARMA(2,3): MSE=0.
+# ARIMA(1,1,1): MSE=0.
+# ARIMA(2,1,2): MSE=0.
